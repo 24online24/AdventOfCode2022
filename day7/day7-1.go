109489f4afbfeb2d7b0c41b74b96508149bbafdd
24online24 <chiuzbaianrares20@gmail.com>
2022-12-09 21:59:07 +0200

    / directory doesn't get updated with nested files for some reason

--- day7/day7-1.go
+++ day7/day7-1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -32,22 +33,33 @@ func main() {
 	for fileScanner.Scan() {
 		lineText := fileScanner.Text()
 		lineTextSeparated := strings.Split(lineText, " ")
-		fmt.Println(lineTextSeparated)
+		// fmt.Println(lineTextSeparated)
 		switch lineTextSeparated[1] {
 		case "cd":
 			directoryIndex = changeDirectory(lineTextSeparated[2])
-			fmt.Println("\t\t\t\t", filesystem[directoryIndex])
+			// fmt.Println(filesystem[directoryIndex])
 		case "ls":
-			lineInBuffer := listChildren(directoryIndex, fileScanner)
-			if lineInBuffer != "" {
-				lineInBufferSeparated := strings.Split(lineInBuffer, " ")
+			lineInBufferSeparated := listChildren(directoryIndex, fileScanner)
+			fmt.Println(filesystem)
+			if len(lineInBufferSeparated) > 0 {
+				// fmt.Println("-------------", lineInBufferSeparated)
 				directoryIndex = changeDirectory(lineInBufferSeparated[2])
+				// fmt.Println(filesystem)
+
+				// fmt.Println("\t", filesystem[directoryIndex])
+
+				// fmt.Println("******************", directoryIndex)
 			}
 		}
 	}
 	fmt.Println(filesystem)
+	for index, dir := range filesystem {
+		fmt.Println(index, dir)
+	}
 }
+
 func changeDirectory(directoryName string) int {
+	// fmt.Println("******************", directoryName)
 	for index, dir := range filesystem {
 		if dir.name == directoryName {
 			return index
@@ -56,11 +68,34 @@ func changeDirectory(directoryName string) int {
 	return -1
 }
 
-func listChildren(directoryIndex int, fileScanner *bufio.Scanner) string {
+func listChildren(directoryIndex int, fileScanner *bufio.Scanner) []string {
 	for fileScanner.Scan() {
 		lineText := fileScanner.Text()
-		if lineText[0] == '$' {
-			return lineText
+		lineTextSeparated := strings.Split(lineText, " ")
+		// fmt.Println(lineText)
+		switch lineTextSeparated[0] {
+		case "$":
+			return lineTextSeparated
+		case "dir":
+			filesystem = append(filesystem, directory{name: lineTextSeparated[1], parent: &filesystem[directoryIndex]})
+			filesystem[directoryIndex].children = append(filesystem[directoryIndex].children, "")
+		default:
+			sizeUpdate, err := strconv.Atoi(lineTextSeparated[0])
+
+			if err != nil {
+				panic("Something went wrong with the size transformation to int!")
+			}
+
+			// filesystem[directoryIndex].size += sizeUpdate
+			directoryToUpdate := &filesystem[directoryIndex]
+			for directoryToUpdate != nil {
+				(*directoryToUpdate).size += sizeUpdate
+				fmt.Println("Updated", directoryToUpdate, "with file", lineTextSeparated, ". Size is now", directoryToUpdate.size)
+				// fmt.Println(filesystem)
+				directoryToUpdate = directoryToUpdate.parent
+			}
 		}
 	}
+	var emptyArr []string
+	return emptyArr
 }
